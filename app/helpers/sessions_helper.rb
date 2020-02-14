08ff958267b9e8dc8f4c9684813001bca1e884b1
izayoi riko <mmoursnuimneg@gmail.com>
2020-02-14 00:39:29 +0000

    7-1 Remember Me 追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,39 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+   # 永続的セッションを記憶します（Userモデルを参照
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # 永続的セッションを破棄します
+  def fotget(user)
+    user.forget # Userモデル参照
+    coolies.delete(:user_id)
+    coolies.delete(:remember_token)
+  end
+  
   # セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # 一時的セッションにいるユーザーを返します。
+  # それ以外の場合はcookiesに対応するユーザーを返します。
+  def current_user
+    if (user_id = session[:usera_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cllkies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   # 現在ログイン中のユーザーがいる場合オブジェクトを返します
