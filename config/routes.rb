b579219eac5460895246446bb458fa06ffcdceb3
izayoi riko <mmoursnuimneg@gmail.com>
2020-02-22 06:51:04 +0000

    11-1-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
+  
   # ログイン機能
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
       get 'edit_basic_info'
       patch 'update_basic_info'
       get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
     end
     resources :attendances, only: :update 
   end
