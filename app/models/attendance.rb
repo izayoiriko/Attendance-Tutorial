dc5ba118359c5d0c17b71d8da68cf59d6297e455
izayoi riko <mmoursnuimneg@gmail.com>
2020-02-22 08:43:41 +0000

    11-3-2

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,7 +8,7 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   
   #出勤・退勤時間どちらも存在するとき、出勤時間より早い退勤時間は無効
-  validate :finished_at_than_finished_at_fast_if_invalid
+  validate :started_at_than_finished_at_fast_if_invalid
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
